4a19bbd882c7b4c498a889f3f8370c43fe4dc0ac
Maria Gudmundsdottir <mariagudmundsdottir@Marias-MacBook-Pro.local>
2018-04-26 11:16:13 +0000

    computer exam

---
+++ SQL/update_computer_exam.sql
@@ -0,0 +1,5 @@
+update courses set computer_exam = true where course_code = 'SE-T-303-VERD';
+update courses set computer_exam = true where course_code = 'SE-T-208-FOR2';
+update courses set computer_exam = true where course_code = 'SC-T-201-GSKI';
+update courses set computer_exam = true where course_code = 'SE-T-650-SUST';
+update courses set computer_exam = true where course_code = 'SC-T-111-PROG';
