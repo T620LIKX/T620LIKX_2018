d9ae2f76d1eca5410b1e2e27a52b711ba5948be1
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-05-04 10:53:45 +0000

    adding timeslot to room assignment solution

--- SQL/update_solutions_and_solution_room_assignment.sql
+++ SQL/update_solutions_and_solution_room_assignment.sql
@@ -4,6 +4,7 @@ DROP COLUMN room_id;
 CREATE TABLE solution_room_assignment (
   id serial,
   solution_id int,
+  timeslot int,
   course_id int references courses(id),
   room_id int references rooms(id),
   student_count int,
