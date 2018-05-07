b7d54c7084c251d1c81bb3015c0752e665d289f0
Ásthildur Lára Stefánsdóttir <asthildurs@Asthildurs-MacBook-2.local>
2018-05-07 11:48:49 +0000

    Hægt að nota bæði fyrir stundatöflu og próftöflu núna, vantar samt skipanir fyrir stundatöflu

--- Optimization/print_solutions_statistics.py
+++ Optimization/print_solutions_statistics.py
@@ -87,198 +87,225 @@ if len(sys.argv) > 2:
     if sys.argv[2] == '1':
         print_details = True
 
-students_in_timeslot = """select s.timeslot, count(e.student_id)
-from courses c, enrollment e, solutions s
+
+# Er þetta próftöflulausn?
+proftafla = False
+proftafla_check = """select * from solutions
 where solution_id = {}
-and c.id = s.course_id
-and c.id = e.course_id
-and s.solution_type = 'examtable'
-group by s.timeslot
-order by s.timeslot;"""
+and solution_type = 'examtable';"""
 
-cursor.execute(students_in_timeslot.format(solution_id))
-total_students_in_timeslot = cursor.fetchall()
+cursor.execute(proftafla_check.format(solution_id))
+total_proftafla_check = cursor.fetchall()
 
+if len(total_proftafla_check) > 0:
+    proftafla = True
 
-if len(students_in_timeslot) == 0:
-    print('No solution found')
-    exit()
+# Er þetta stundatöflulausn?
+stundatafla = False
+stundatafla_check = """select * from solutions
+where solution_id = {}
+and solution_type = 'timetable';"""
 
+cursor.execute(stundatafla_check.format(solution_id))
+total_stundatafla_check = cursor.fetchall()
 
-headline = "Fjöldi nemenda í prófi í hverju tímaslotti:"
-print(headline)
-thetable, slots, days = create_table_exams(total_students_in_timeslot)
-print_solution(thetable, slots, days)
+if len(total_stundatafla_check) > 0:
+    stundatafla = True
 
-
-total_students = """ select count(student_id) from
-  (SELECT DISTINCT e.student_id
-FROM enrollment e, courses c, solutions s
-WHERE s.solution_id = 2
-AND e.course_id = s.course_id
-AND e.course_id = c.id
-GROUP BY e.student_id
-ORDER BY e.student_id) tmp"""
-
-cursor.execute(total_students.format(solution_id))
-total_students_in_table= cursor.fetchall()
-
-print('Heildarfjöldi nemenda sem skráðir eru í lokapróf:', total_students_in_table[0][0])
-
-#skörun í sama tímaslotti
-
-student_skorun = """select e1.student_id, c1.course_code, c2.course_code, s1.timeslot
-from enrollment e1, enrollment e2, courses c1, courses c2, solutions s1, solutions s2
-where s1.solution_id = {}
-and s2.solution_id = s1.solution_id
-and e1.student_id = e2.student_id
-and e1.course_id < e2.course_id
-and e1.course_id = s1.course_id and e2.course_id = s2.course_id
-and e1.course_id = c1.id and e2.course_id = c2.id
-and s1.timeslot = s2.timeslot
-group by e1.student_id, c1.course_code, c2.course_code, s1.timeslot
-order by e1.student_id"""
-
-cursor.execute(student_skorun.format(solution_id))
-total_students_skorun = cursor.fetchall()
-
-counter = 0
-for x in total_students_skorun:
-    counter += 1
-
-print('Fjöldi nemenda sem skráðir eru í próf í sama tímaslotti:', counter)
-
-
-#próf fyrir og eftir hádegi
-
-student_skorun_fyrir_og_eftir_hadegi = """select e1.student_id, c1.course_code, s1.timeslot, c2.course_code, s2.timeslot
-from enrollment e1, enrollment e2, courses c1, courses c2, solutions s1, solutions s2
-where s1.solution_id = {}
-and s2.solution_id = s1.solution_id
-and e1.student_id = e2.student_id
-and e1.course_id < e2.course_id
-and e1.course_id = s1.course_id and e2.course_id = s2.course_id
-and e1.course_id = c1.id and e2.course_id = c2.id
-and s1.timeslot + 1 = s2.timeslot
-and s2.timeslot%2 = 0
-group by e1.student_id, c1.course_code, s1.timeslot, c2.course_code, s2.timeslot
-order by e1.student_id"""
-
-
-cursor.execute(student_skorun_fyrir_og_eftir_hadegi.format(solution_id))
-total_student_skorun_fyrir_og_eftir_hadegi = cursor.fetchall()
-
-counter = 0
-for x in total_student_skorun_fyrir_og_eftir_hadegi:
-    counter +=1
-
-print('\n')
-print('Fjöldi nemenda sem eru skráðir í próf fyrir og eftir hádegi sama dags: ', counter)
-
-select_solution = """select s.course_id, c.course_code, d.department_name, s.timeslot, s.solution_type, e.student_id
-from solutions s, courses c, departments d, enrollment e
-where s.course_id = c.id and c.department_id = d.id and c.id = e.course_id
-and s.solution_id = {} and e.student_id = {}
-order by d.department_name, s.timeslot, s.course_id, e.student_id;"""
-
-
-select_students = """select e.student_id
-from enrollment e, solutions s, courses c
-where s.course_id = c.id and c.id = e.course_id
-and s.solution_id = {}
-group by e.student_id;"""
-
-cursor.execute(select_students.format(solution_id))
-thestudents = cursor.fetchall()
-
-fjoldi_nem_2daga_i_rod = 0
-fjoldi_nem_3daga_i_rod = 0
-fjoldi_nem_4daga_i_rod = 0
-fjoldi_nem_5daga_i_rod = 0
-
-for i in thestudents:
-    cursor.execute(select_solution.format(solution_id, i[0]))
-    thesolutions = cursor.fetchall()
-
-    thetable, slots, days = create_table(thesolutions)
-
-    this_student_is_3days_in_row = False
-    for theday in [0,1,2,5,6,7]:
-        if (len(thetable[theday][0]+thetable[theday][1]) > 0) and (len(thetable[theday+1][0]+thetable[theday+1][1]) > 0) and (len(thetable[theday+2][0]+thetable[theday+2][1]) > 0):
-            this_student_is_3days_in_row = True
-
-    if this_student_is_3days_in_row:
-        fjoldi_nem_3daga_i_rod += 1
-
-        #print_solution(thetable, slots, days)
-
-    this_student_is_4days_in_row = False
-    for theday in [0,1,5,6]:
-        if (len(thetable[theday][0]+thetable[theday][1]) > 0) and (len(thetable[theday+1][0]+thetable[theday+1][1]) > 0) and (len(thetable[theday+2][0]+thetable[theday+2][1]) > 0) and (len(thetable[theday+3][0]+thetable[theday+3][1]) > 0):
-            this_student_is_4days_in_row = True
-
-    if this_student_is_4days_in_row:
-        fjoldi_nem_4daga_i_rod += 1
-
-        #print_solution(thetable, slots, days)
-
-    this_student_is_5days_in_row = False
-    for theday in [0,5]:
-        if (len(thetable[theday][0]+thetable[theday][1]) > 0) and (len(thetable[theday+1][0]+thetable[theday+1][1]) > 0) and (len(thetable[theday+2][0]+thetable[theday+2][1]) > 0) and (len(thetable[theday+3][0]+thetable[theday+3][1]) > 0) and (len(thetable[theday+4][0]+thetable[theday+4][1]) > 0):
-            this_student_is_5days_in_row = True
-
-    if this_student_is_5days_in_row:
-        fjoldi_nem_5daga_i_rod += 1
-
-        #print_solution(thetable, slots, days)
-
-    this_student_is_2days_in_row = False
-    for theday in [0,1,2,3,5,6,7,8]:
-        if (len(thetable[theday][0]+thetable[theday][1]) > 0) and (len(thetable[theday+1][0]+thetable[theday+1][1]) > 0):
-            this_student_is_2days_in_row = True
-
-    if this_student_is_2days_in_row:
-        fjoldi_nem_2daga_i_rod += 1
-
-        #print_solution(thetable, slots, days)
-
-print('\n')
-
-print('Fjöldi nemenda í tveimur eða fleiri prófum 2 daga í röð: {}'.format(fjoldi_nem_2daga_i_rod))
-print('Fjöldi nemenda í þremur eða fleiri prófum 3 daga í röð: {}'.format(fjoldi_nem_3daga_i_rod))
-print('Fjöldi nemenda í fjórum eða fleiri prófum 4 daga í röð: {}'.format(fjoldi_nem_4daga_i_rod))
-print('Fjöldi nemenda í fimm eða fleiri prófum 5 daga í röð: {}'.format(fjoldi_nem_5daga_i_rod))
-
-
-#Fjöldi nemenda í hverju lokaprófi
-if print_details == True:
-    student_taking_exam = """select c.course_code, c.course_name, s.timeslot, count(e.student_id)
+# Proftöflu prentun
+if proftafla == True:
+    students_in_timeslot = """select s.timeslot, count(e.student_id)
     from courses c, enrollment e, solutions s
     where solution_id = {}
     and c.id = s.course_id
     and c.id = e.course_id
-    group by c.course_code, c.course_name, s.timeslot
+    group by s.timeslot
     order by s.timeslot;"""
 
-    cursor.execute(student_taking_exam.format(solution_id))
-    total_students_taking_exam = cursor.fetchall()
+    cursor.execute(students_in_timeslot.format(solution_id))
+    total_students_in_timeslot = cursor.fetchall()
 
-    print('\n')
-    print('Fjöldi nemenda í hverju lokaprófi')
-    for x in total_students_taking_exam:
-        print(x[0], '-', x[1], '- fjöldi nemanda:', x[3])
+    headline = "Fjöldi nemenda í prófi í hverju tímaslotti:"
+    print(headline)
+    thetable, slots, days = create_table_exams(total_students_in_timeslot)
+    print_solution(thetable, slots, days)
 
-    print('\n')
-    print('Nemendur sem skráðir eru í tvö próf á sama tíma:')
+
+    total_students = """ select count(student_id) from
+      (SELECT DISTINCT e.student_id
+    FROM enrollment e, courses c, solutions s
+    WHERE s.solution_id = 2
+    AND e.course_id = s.course_id
+    AND e.course_id = c.id
+    GROUP BY e.student_id
+    ORDER BY e.student_id) tmp"""
+
+    cursor.execute(total_students.format(solution_id))
+    total_students_in_table= cursor.fetchall()
+
+    print('Heildarfjöldi nemenda sem skráðir eru í lokapróf:', total_students_in_table[0][0])
+
+    #skörun í sama tímaslotti
+
+    student_skorun = """select e1.student_id, c1.course_code, c2.course_code, s1.timeslot
+    from enrollment e1, enrollment e2, courses c1, courses c2, solutions s1, solutions s2
+    where s1.solution_id = {}
+    and s2.solution_id = s1.solution_id
+    and e1.student_id = e2.student_id
+    and e1.course_id < e2.course_id
+    and e1.course_id = s1.course_id and e2.course_id = s2.course_id
+    and e1.course_id = c1.id and e2.course_id = c2.id
+    and s1.timeslot = s2.timeslot
+    group by e1.student_id, c1.course_code, c2.course_code, s1.timeslot
+    order by e1.student_id"""
+
+    cursor.execute(student_skorun.format(solution_id))
+    total_students_skorun = cursor.fetchall()
+
+    counter = 0
     for x in total_students_skorun:
-        print('Student ID:', x[0], '- áfangar sem skarast:', x[1], 'og', x[2], '- tímaslott:', x[3])
+        counter += 1
+
+    print('Fjöldi nemenda sem skráðir eru í próf í sama tímaslotti:', counter)
+
+
+    #próf fyrir og eftir hádegi
+
+    student_skorun_fyrir_og_eftir_hadegi = """select e1.student_id, c1.course_code, s1.timeslot, c2.course_code, s2.timeslot
+    from enrollment e1, enrollment e2, courses c1, courses c2, solutions s1, solutions s2
+    where s1.solution_id = {}
+    and s2.solution_id = s1.solution_id
+    and e1.student_id = e2.student_id
+    and e1.course_id < e2.course_id
+    and e1.course_id = s1.course_id and e2.course_id = s2.course_id
+    and e1.course_id = c1.id and e2.course_id = c2.id
+    and s1.timeslot + 1 = s2.timeslot
+    and s2.timeslot%2 = 0
+    group by e1.student_id, c1.course_code, s1.timeslot, c2.course_code, s2.timeslot
+    order by e1.student_id"""
+
+
+    cursor.execute(student_skorun_fyrir_og_eftir_hadegi.format(solution_id))
+    total_student_skorun_fyrir_og_eftir_hadegi = cursor.fetchall()
+
+    counter = 0
+    for x in total_student_skorun_fyrir_og_eftir_hadegi:
+        counter +=1
 
     print('\n')
-    print('Nemendur sem skráðir eru í próf fyrir og eftir hádegi sama dags:')
-    for x in total_student_skorun_fyrir_og_eftir_hadegi:
-        print('Student ID: ', x[0], '- áfangar sem skarast:' ,x[1] , 'og', x[3], '- tímaslott:', x[2], 'og', x[4])
+    print('Fjöldi nemenda sem eru skráðir í próf fyrir og eftir hádegi sama dags: ', counter)
+
+    select_solution = """select s.course_id, c.course_code, d.department_name, s.timeslot, s.solution_type, e.student_id
+    from solutions s, courses c, departments d, enrollment e
+    where s.course_id = c.id and c.department_id = d.id and c.id = e.course_id
+    and s.solution_id = {} and e.student_id = {}
+    order by d.department_name, s.timeslot, s.course_id, e.student_id;"""
+
+
+    select_students = """select e.student_id
+    from enrollment e, solutions s, courses c
+    where s.course_id = c.id and c.id = e.course_id
+    and s.solution_id = {}
+    group by e.student_id;"""
+
+    cursor.execute(select_students.format(solution_id))
+    thestudents = cursor.fetchall()
+
+    fjoldi_nem_2daga_i_rod = 0
+    fjoldi_nem_3daga_i_rod = 0
+    fjoldi_nem_4daga_i_rod = 0
+    fjoldi_nem_5daga_i_rod = 0
+
+    for i in thestudents:
+        cursor.execute(select_solution.format(solution_id, i[0]))
+        thesolutions = cursor.fetchall()
+
+        thetable, slots, days = create_table(thesolutions)
+
+        this_student_is_3days_in_row = False
+        for theday in [0,1,2,5,6,7]:
+            if (len(thetable[theday][0]+thetable[theday][1]) > 0) and (len(thetable[theday+1][0]+thetable[theday+1][1]) > 0) and (len(thetable[theday+2][0]+thetable[theday+2][1]) > 0):
+                this_student_is_3days_in_row = True
+
+        if this_student_is_3days_in_row:
+            fjoldi_nem_3daga_i_rod += 1
+
+            #print_solution(thetable, slots, days)
+
+        this_student_is_4days_in_row = False
+        for theday in [0,1,5,6]:
+            if (len(thetable[theday][0]+thetable[theday][1]) > 0) and (len(thetable[theday+1][0]+thetable[theday+1][1]) > 0) and (len(thetable[theday+2][0]+thetable[theday+2][1]) > 0) and (len(thetable[theday+3][0]+thetable[theday+3][1]) > 0):
+                this_student_is_4days_in_row = True
+
+        if this_student_is_4days_in_row:
+            fjoldi_nem_4daga_i_rod += 1
+
+            #print_solution(thetable, slots, days)
+
+        this_student_is_5days_in_row = False
+        for theday in [0,5]:
+            if (len(thetable[theday][0]+thetable[theday][1]) > 0) and (len(thetable[theday+1][0]+thetable[theday+1][1]) > 0) and (len(thetable[theday+2][0]+thetable[theday+2][1]) > 0) and (len(thetable[theday+3][0]+thetable[theday+3][1]) > 0) and (len(thetable[theday+4][0]+thetable[theday+4][1]) > 0):
+                this_student_is_5days_in_row = True
+
+        if this_student_is_5days_in_row:
+            fjoldi_nem_5daga_i_rod += 1
+
+            #print_solution(thetable, slots, days)
+
+        this_student_is_2days_in_row = False
+        for theday in [0,1,2,3,5,6,7,8]:
+            if (len(thetable[theday][0]+thetable[theday][1]) > 0) and (len(thetable[theday+1][0]+thetable[theday+1][1]) > 0):
+                this_student_is_2days_in_row = True
+
+        if this_student_is_2days_in_row:
+            fjoldi_nem_2daga_i_rod += 1
+
+            #print_solution(thetable, slots, days)
+
+    print('\n')
+
+    print('Fjöldi nemenda í tveimur eða fleiri prófum 2 daga í röð: {}'.format(fjoldi_nem_2daga_i_rod))
+    print('Fjöldi nemenda í þremur eða fleiri prófum 3 daga í röð: {}'.format(fjoldi_nem_3daga_i_rod))
+    print('Fjöldi nemenda í fjórum eða fleiri prófum 4 daga í röð: {}'.format(fjoldi_nem_4daga_i_rod))
+    print('Fjöldi nemenda í fimm eða fleiri prófum 5 daga í röð: {}'.format(fjoldi_nem_5daga_i_rod))
+
+
+    #Fjöldi nemenda í hverju lokaprófi
+    if print_details == True:
+        student_taking_exam = """select c.course_code, c.course_name, s.timeslot, count(e.student_id)
+        from courses c, enrollment e, solutions s
+        where solution_id = {}
+        and c.id = s.course_id
+        and c.id = e.course_id
+        group by c.course_code, c.course_name, s.timeslot
+        order by s.timeslot;"""
+
+        cursor.execute(student_taking_exam.format(solution_id))
+        total_students_taking_exam = cursor.fetchall()
+
+        print('\n')
+        print('Fjöldi nemenda í hverju lokaprófi')
+        for x in total_students_taking_exam:
+            print(x[0], '-', x[1], '- fjöldi nemanda:', x[3])
+
+        print('\n')
+        print('Nemendur sem skráðir eru í tvö próf á sama tíma:')
+        for x in total_students_skorun:
+            print('Student ID:', x[0], '- áfangar sem skarast:', x[1], 'og', x[2], '- tímaslott:', x[3])
+
+        print('\n')
+        print('Nemendur sem skráðir eru í próf fyrir og eftir hádegi sama dags:')
+        for x in total_student_skorun_fyrir_og_eftir_hadegi:
+            print('Student ID: ', x[0], '- áfangar sem skarast:' ,x[1] , 'og', x[3], '- tímaslott:', x[2], 'og', x[4])
+
+elif stundatafla == True:
+    print('Stundatafla')
+
+else:
+    print('No solution found')
+    exit()
 
 
 
-conn.close()
+    conn.close()
 
