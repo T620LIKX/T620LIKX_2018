eb69c7bc2c990f897341d128153601f3cdbfcbdc
Brynja Rán Guðmundsdóttir <BrynjaRan@Brynjas-MacBook-Air.local>
2018-05-09 11:07:07 +0000

    Uppfært

--- SQL/update_fjarnam_vidskiptafr.sql
+++ SQL/update_fjarnam_vidskiptafr.sql
@@ -1,10 +1,8 @@
-update courses set final_exam = false
+Update courses set final_exam = FALSE
 where course_name like '%fjarnám%'
 and semester_type = 20184;
 
-update enrollment set course_id = 469 where course_id = 468
-update enrollment set course_id = 463 where course_id = 462
-update enrollment set course_id = 487 where course_id = 488
-
-
-
+Update enrollment set course_id  = 469 where course_id = 468;
+Update enrollment set course_id  = 463 where course_id = 462;
+Update enrollment set course_id  = 487 where course_id = 488
+and student_id not in (620, 3379);
