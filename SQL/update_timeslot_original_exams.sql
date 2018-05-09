f0b2101d1626acccc69a539d08dcbf2311ed35b6
Brynja Rán Guðmundsdóttir <BrynjaRan@Brynjas-MacBook-Air.local>
2018-05-09 10:05:25 +0000

    update timeslot in original exams

---
+++ SQL/update_timeslot_original_exams.sql
@@ -0,0 +1,79 @@
+UPDATE original_exams set timeslot = 1
+where day_of_exam = '09.04.2018'
+and starting = '09:00';
+
+UPDATE original_exams set timeslot = 2
+where day_of_exam = '09.04.2018'
+and starting = '14:00';
+
+UPDATE original_exams set timeslot = 3
+where day_of_exam = '10.04.2018'
+and starting = '09:00';
+
+UPDATE original_exams set timeslot = 4
+where day_of_exam = '10.04.2018'
+and starting = '14:00';
+
+UPDATE original_exams set timeslot = 5
+where day_of_exam = '11.04.2018'
+and starting = '09:00';
+
+UPDATE original_exams set timeslot = 6
+where day_of_exam = '11.04.2018'
+and starting = '14:00';
+
+UPDATE original_exams set timeslot = 7
+where day_of_exam = '12.04.2018'
+and starting = '09:00';
+
+UPDATE original_exams set timeslot = 8
+where day_of_exam = '12.04.2018'
+and starting = '14:00';
+
+UPDATE original_exams set timeslot = 9
+where day_of_exam = '13.04.2018'
+and starting = '09:00';
+
+UPDATE original_exams set timeslot = 10
+where day_of_exam = '13.04.2018'
+and starting = '14:00';
+
+UPDATE original_exams set timeslot = 11
+where day_of_exam = '16.04.2018'
+and starting = '09:00';
+
+UPDATE original_exams set timeslot = 12
+where day_of_exam = '16.04.2018'
+and starting = '14:00';
+
+UPDATE original_exams set timeslot = 13
+where day_of_exam = '17.04.2018'
+and starting = '09:00';
+
+UPDATE original_exams set timeslot = 14
+where day_of_exam = '17.04.2018'
+and starting = '14:00';
+
+UPDATE original_exams set timeslot = 15
+where day_of_exam = '18.04.2018'
+and starting = '09:00';
+
+UPDATE original_exams set timeslot = 16
+where day_of_exam = '18.04.2018'
+and starting = '14:00';
+
+UPDATE original_exams set timeslot = 17
+where day_of_exam = '19.04.2018'
+and starting = '09:00';
+
+UPDATE original_exams set timeslot = 18
+where day_of_exam = '19.04.2018'
+and starting = '14:00';
+
+UPDATE original_exams set timeslot = 19
+where day_of_exam = '20.04.2018'
+and starting = '09:00';
+
+UPDATE original_exams set timeslot = 20
+where day_of_exam = '20.04.2018'
+and starting = '14:00';
