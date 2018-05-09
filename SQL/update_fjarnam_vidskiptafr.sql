47e2e9dcbf370400a990e2abd38aedb070f38496
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-05-09 09:59:36 +0000

    update for proftoflu comparison

---
+++ SQL/update_fjarnam_vidskiptafr.sql
@@ -0,0 +1,10 @@
+update courses set final_exam = false
+where course_name like '%fjarnám%'
+and semester_type = 20184;
+
+update enrollment set course_id = 469 where course_id = 468
+update enrollment set course_id = 463 where course_id = 462
+update enrollment set course_id = 487 where course_id = 488
+
+
+
